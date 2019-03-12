2c31c823f8ab4a4e8867d6e388b0663f76166bfc
Stéphane Wirtel <stephane@wirtel.be>
2019-03-13 00:37:02 +0100

    Add Irish as prefix of Python, just for the fun and the presentation of the Meetup

--- Modules/main.c
+++ Modules/main.c
@@ -481,7 +481,7 @@ pymain_header(const _PyCoreConfig *config)
         return;
     }
 
-    fprintf(stderr, "Python %s on %s\n", Py_GetVersion(), Py_GetPlatform());
+    fprintf(stderr, "IrishPython %s on %s\n", Py_GetVersion(), Py_GetPlatform());
     if (config->site_import) {
         fprintf(stderr, "%s\n", COPYRIGHT);
     }
